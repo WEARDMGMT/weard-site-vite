5183f1d6874a3d5c6430db4b1f318113088dd515
WEARDMGMT <james@weardmgmt.com>
2025-08-31 17:39:22 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './weard/App.jsx'
 import './styles.css'
-import { HelmetProvider } from 'react-helmet-async'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
+    <App />
   </React.StrictMode>
 )
