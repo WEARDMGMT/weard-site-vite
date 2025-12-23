849f98a7e0dc3a550dd1e7a58a828f9baf0ffd9a
WEARDMGMT <james@weardmgmt.com>
2025-12-23 18:00:57 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/weard-site-vite/',
   plugins: [react()],
-  base: '/', // <-- this is the only new line
 })
